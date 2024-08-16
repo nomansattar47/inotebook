7923adfac36b0ea87509df13d2b86b91b09c0cc6
Noman Sattar <77478126+nomansattar47@users.noreply.github.com>
2024-08-16 19:50:18 +0500

    hashing password using bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express');
 const User = require('../models/User')
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_TOKEN_KEY = 'N0mAnBaNYGa$';
 
 
 // Create a User using: POST "/api/auth/createuser" No login required
@@ -26,13 +29,26 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+        // Create a new User
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
         })
 
-        res.json(user);
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_TOKEN_KEY);
+        // console.log(jwtData);
+
+        // res.json(user);
+        res.json({authToken});
 
     } catch (error) {
         console.error(error.message);
