51bb85569b9340b26ffba75f1c3e9fbbe9dba807
Noman Sattar <77478126+nomansattar47@users.noreply.github.com>
2024-08-22 19:49:40 +0500

    added first context api
    useContext hook using react context api

--- src/App.js
+++ src/App.js
@@ -7,21 +7,20 @@ import {
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-    
-      <Router>
-      <Navbar/>
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/about" element={<About/>} />
-          {/* <Route exact path="/" element={<News country="in" pageSize="7" category="general" setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />}></Route> */}
-        </Routes>
-        
-        
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
